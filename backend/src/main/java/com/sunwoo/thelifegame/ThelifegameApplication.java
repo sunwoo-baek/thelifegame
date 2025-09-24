4d36f99f850aebb2eb4a908800ed2535769a3489
Sunwoo <sunwooo0728@gmail.com>
2025-09-24 00:31:46 -0700

    Initialized Spring boot with Spring Initializr

---
+++ backend/src/main/java/com/sunwoo/thelifegame/ThelifegameApplication.java
@@ -0,0 +1,13 @@
+package com.sunwoo.thelifegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThelifegameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThelifegameApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sunwoo/thelifegame/ThelifegameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunwoo.thelifegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThelifegameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
